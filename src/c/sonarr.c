d13e320066fb429947bf1820c9ca7b8620b3fa07
Mike Zeller <mike.zeller@joyent.com>
2016-09-04 21:58:48 -0700

    start working on app

--- src/c/sonarr.c
+++ src/c/sonarr.c
@@ -1,60 +1,55 @@
 #include <pebble.h>
 
-static Window *s_window;
+static Window *s_main_window;
 static TextLayer *s_text_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
-}
-
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
-}
-
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
-}
-
-static void prv_window_load(Window *window) {
+static void main_window_load(Window *window) {
+  // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  // Create the TextLayer with specific bounds
+  s_text_layer = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+
+    // Improve the layout to be more like a watchface
+    text_layer_set_background_color(s_text_layer, GColorClear);
+    text_layer_set_text_color(s_text_layer, GColorBlack);
+    text_layer_set_text(s_text_layer, "Sonarr");
+    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+
+    // Add it as a child layer to the Window's root layer
+    layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  }
+
+static void main_window_unload(Window *window) {
+
 }
 
-static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
-}
+static void prv_init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
 
-static void prv_init(void) {
-  s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
   });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
 }
 
-static void prv_deinit(void) {
-  window_destroy(s_window);
+static void prv_deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
 }
 
-int main(void) {
+int main() {
   prv_init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
   app_event_loop();
+
   prv_deinit();
 }
